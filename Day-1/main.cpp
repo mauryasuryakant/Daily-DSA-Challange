a74a311e1d5c3d70dd0b7a8c364c4c545c12bbaf
Maurya Suryakant <mauryasuryakant99@gmail.com>
2025-10-21 21:59:31 +0530

    Completed code

--- Day-1/main.cpp
+++ Day-1/main.cpp
@@ -1,24 +1,27 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    int n;
-    cout << "Enter size of array: ";
-    cin >> n;
+int main()
+{
+    int number;
+    cout << "Enter the array here ";
+    cin >> number;
 
-    int arr[n];
-    cout << "Enter " << n << " elements: ";
-    for (int i = 0; i < n; i++) {
+    int arr[number];
+    cout << "Enter " << number << " numbers ";
+    for (int i = 0; i < number; i++)
+    {
         cin >> arr[i];
     }
 
     int max = arr[0];
-    for (int i = 1; i < n; i++) {
-        if (arr[i] > max)
+    for(int i = 1; i < number; i++){
+        if(arr[i] > max){
             max = arr[i];
+        }
     }
 
-    cout << "The largest element is: " << max << endl;
-
+    cout << "The largest element in array is " << max << endl;
+    system("pause");
     return 0;
 }
