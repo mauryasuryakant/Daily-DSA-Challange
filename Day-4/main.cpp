b95bf1f5ed742d3901930a242623a8c4fa49dee6
Maurya Suryakant <mauryasuryakant99@gmail.com>
2025-10-24 09:40:56 +0530

    Refactor code structure for improved readability and maintainability

---
+++ Day-4/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int main()
+{
+
+    vector<int> arr = {4, 1, 6, 2, 5};
+
+    for (int val : arr)
+    {
+        cout << val << " ";
+    }
+    cout << endl;
+
+    sort(arr.begin(), arr.end());
+
+    cout << "Ascending array :- ";
+
+    for (int val : arr)
+    {
+        cout << val << " ";
+    }
+    cout << endl;
+
+    sort(arr.begin(), arr.end(), greater<int>());
+
+    cout << "Descending array :- ";
+
+    for (int val : arr)
+    {
+        cout << val << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
