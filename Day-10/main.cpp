c005c33fb06fe46619d62af9415de898d3a1fa80
Maurya Suryakant <mauryasuryakant99@gmail.com>
2025-11-01 19:45:45 +0530

    Completed Day - 10

--- Day-10/main.cpp
+++ Day-10/main.cpp
@@ -1,124 +1,42 @@
+// Merge Two Sorted Arrays
+
 #include <iostream>
 #include <vector>
 
 int main () {
 
-    std::vector<int> arr = {1, 2, 4, 75, 33};
+    std::vector<int> array_1 = {6, 7, 8, 9, 0};
+    std::vector<int> array_2 = {1, 2, 3, 4, 5};
+    std::vector<int> sorted;
 
-    int n = sizeof(arr)/sizeof(arr[0]);
+    int n1 = array_1.size();
+    int n2 = array_2.size();
+
+    for(int i = 0; i < n1; i++){
+        sorted.push_back(array_1[i]);
+    }
+
+    for(int i = 0; i < n2; i++){
+        sorted.push_back(array_2[i]);
+    }
+
+    int n = sorted.size();
 
     for(int i = 0; i < n; i++){
         for(int j = 0; j < n - 1; j++){
-                if(arr[j] > arr[j+1]){
-                    int temp_var = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = temp_var;
-                }
+            if(sorted[j] > sorted[j+1]){
+                int temp_var = sorted[j];
+                sorted[j] = sorted[j + 1];
+                sorted[j + 1] = temp_var;
             }
         }
+    }
 
-    for(int val : arr){
+    for(int val : sorted){
         std::cout << val << " ";
     }
 
     std::cout << std::endl;
 
-    return false;
+    return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// // Merge Two Sorted Arrays
-
-// #include <iostream>
-// #include <vector>
-
-// int main () {
-
-//     std::vector<int> array_1 = {1, 2, 3, 4, 5};
-//     std::vector<int> array_2 = {1, 2, 3, 4, 5};
-//     std::vector<int> sorted;
-
-//     int n1 = array_1.size();
-//     int n2 = array_2.size();
-
-//     for(int i = 0; i < n1; i++){
-//         sorted.push_back(array_1[i]);
-//     }
-
-//     for(int i = 0; i < n2; i++){
-//         sorted.push_back(array_2[i]);
-//     }
-
-//     for(int val : sorted){
-//         std::cout << val << " ";
-//     }
-
-//     std::cout << std::endl;
-
-//     return 0;
-// }
