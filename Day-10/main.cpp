90bc9af938c4f13a15390e10384e690ce15b8f9a
Maurya Suryakant <mauryasuryakant99@gmail.com>
2025-11-01 19:28:05 +0530

    Fixing Day - 10

--- Day-10/main.cpp
+++ Day-10/main.cpp
@@ -3,26 +3,122 @@
 
 int main () {
 
-    std::vector<int> array_1 = {1, 2, 3, 4, 5};
-    std::vector<int> array_2 = {1, 2, 3, 4, 5};
-    std::vector<int> sorted;
+    std::vector<int> arr = {1, 2, 4, 75, 33};
 
-    int n1 = array_1.size();
-    int n2 = array_2.size();
+    int n = sizeof(arr)/sizeof(arr[0]);
 
-    for(int i = 0; i < n1; i++){
-        sorted.push_back(array_1[i]);
-    }
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n - 1; j++){
+                if(arr[j] > arr[j+1]){
+                    int temp_var = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp_var;
+                }
+            }
+        }
 
-    for(int i = 0; i < n2; i++){
-        sorted.push_back(array_2[i]);
-    }
-
-    for(int val : sorted){
+    for(int val : arr){
         std::cout << val << " ";
     }
 
     std::cout << std::endl;
 
-    return 0;
+    return false;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // Merge Two Sorted Arrays
+
+// #include <iostream>
+// #include <vector>
+
+// int main () {
+
+//     std::vector<int> array_1 = {1, 2, 3, 4, 5};
+//     std::vector<int> array_2 = {1, 2, 3, 4, 5};
+//     std::vector<int> sorted;
+
+//     int n1 = array_1.size();
+//     int n2 = array_2.size();
+
+//     for(int i = 0; i < n1; i++){
+//         sorted.push_back(array_1[i]);
+//     }
+
+//     for(int i = 0; i < n2; i++){
+//         sorted.push_back(array_2[i]);
+//     }
+
+//     for(int val : sorted){
+//         std::cout << val << " ";
+//     }
+
+//     std::cout << std::endl;
+
+//     return 0;
+// }
--- Day-11/main.cpp
+++ Day-11/main.cpp
@@ -3,29 +3,9 @@
 
 int main()
 {
+    std::vector<int> array;
+    std::vector<int> arr;
 
-    std::vector<int> array = {1, 2, 3, 4, 5};
-    std::vector<int> prefix;
 
-    int size1 = sizeof(array) * sizeof(array[0]) / 2;
-    int size2 = sizeof(prefix) * sizeof(prefix[0]) / 2;
-    int n = array.size();
-
-    if (prefix > array)
-    {
-        for (int i = 0; n < i; i++)
-        {
-            prefix.push_back(prefix[i] += array[i - 1] + array[i]);
-        }
-
-        for (int val : prefix)
-        {
-            std::cout << val << " ";
-        }
-    }
-    else
-    {
-        std::cout << "Size is less" << std::endl;
-    }
     return 0;
 }
