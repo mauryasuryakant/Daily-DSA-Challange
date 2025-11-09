030459d6f7e077a046f487bf18f8856440bf7e71
Maurya Suryakant <mauryasuryakant99@gmail.com>
2025-11-09 19:58:44 +0530

    Made more prettier version

--- Daily-DSA/Day-20/LinearSearch.cpp
+++ Daily-DSA/Day-20/LinearSearch.cpp
@@ -12,21 +12,14 @@ int main () {
     int n = array.size();
     int count = 0;
     
-    for(int i = 0; i < n; i++){
-        sum += array[i];
-    }
+    for(int i = 0; i < n; i++) sum += array[i];
 
     int average = sum / n;
     // double average = static_cast<double>(sum)/n;
 
-    for(int i = 0; i < n; i++){
-        if(average < array[i]){
-            count++;
-        }
-    }
+    for(int i = 0; i < n; i++) if(average < array[i]) count++;
 
     std::cout << "Count :- " << count;
-
 
     return 0;
 }
---
+++ GoodToTry.cpp
@@ -0,0 +1,2 @@
+// #include <bits/stdc++.h>
+// using namespace std;int main(){vector<int>a={1,2,2,3,4},b={2,2,3,5},r;sort(a.begin(),a.end());sort(b.begin(),b.end());set_intersection(a.begin(),a.end(),b.begin(),b.end(),back_inserter(r));for(int x:r)cout<<x<<' ';}
